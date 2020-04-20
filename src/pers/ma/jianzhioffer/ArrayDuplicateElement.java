00f8871316b6ae4807191c476aa13e184e86eaf4
mabc666 <1014131074@qq.com>
2020-04-20 16:48:31 +0800

    完成剑指面试题4

--- src/pers/ma/jianzhioffer/ArrayDuplicateElement.java
+++ src/pers/ma/jianzhioffer/ArrayDuplicateElement.java
@@ -3,8 +3,22 @@ package pers.ma.jianzhioffer;
 import java.util.*;
 
 /**
- * 统计整型数组中的重复元素数据并以List集合方式返回重复元素
- * 时间复杂度O（n），空间复杂度O（1）
+ * 面试题3
+ *
+ * 问题：统计整型数组中的重复元素数据并以List集合方式返回重复元素，该数组大小为n其中的整数只能在[0,n)之间。
+ *
+ * 例子：
+ * 在下面的数组中查找重复元素
+ * [0,1,2,3,3,4,4,5,6,7]
+ *
+ * 解决方案：
+ * 假设该数组中所有元素如果都不重复的话将其进行排序排序结果应该是所有的数组中的数据都与下标对应，
+ * 我们从前往后扫描，对数组中的数字进行比较，如果与该数组下标对应我们就什么都不做，
+ * 如果碰到其中的数字在其对应的位置上已经有过了，那么说明这是一个重复的数字，那我们将其记录下来。
+ * 最后如果我们发现当前数组中的数字与下标不同，并且与对应的位置上的数字也不同的话那我们就要交换他们。
+ *
+ * 复杂度：
+ * 时间复杂度O(n)，空间复杂度O(1)
  *
  * @author mabc
  * @date 2020/4/20
@@ -14,23 +28,28 @@ public class ArrayDuplicateElement {
         //创建返回的重复元素数组
         List<Integer> result =  new ArrayList<Integer>();
 
-        //该题题解思路来自剑指OFFER，假设该数组中所有元素如果都不重复的话排序完，每个元素应该就在数组对应的下标位置上，
-        //我们从前往后扫描，进行比较交换将其交换到数组对应的位置上，这样做的们可以边排序边进行比较。整体的时间复杂度在O（N）空间复杂度为O（1）
+        //对数组进行从前往后的扫描
         for (int i = 0; i < nums.length; i++) {
 
             if (nums[i] == i) {
-                //case1：判断当前数组中的数据是否和下标相同，如果相同直接往下扫
+                // case1：判断当前数组中的数据是否和下标相同，如果相同直接往下扫
                 System.out.println("the element is match the nums[" + i + "] is " + nums[i] + " and the i is " + i);
                 continue;
 
             } else if (nums[nums[i]] == nums[i]) {
-                //case2：判断当前指向的数据是否已经在数组中对应的位置，如果是那么这就是一个重复元素
+                // case2：判断当前指向的数据是否已经在数组中对应的位置，如果是那么这就是一个重复元素
                 System.out.println("find dup element the nums[" + i + "] is " +  nums[i] + " the nums[" + nums[i] + "] is " + nums[nums[i]]);
                 result.add(nums[i]);
 
             } else {
-                //case3：该情况是当前数组的元素和这个元素相同的数组下标所对应的元素不相同的情况下，我们要将其进行交换，将当前数据放置到它应该在的位置
+                // case3：该情况是当前数组的元素和这个元素相同的数组下标所对应的元素不相同的情况下，我们要将其进行交换，将当前数据放置到它应该在的位置
                 System.out.println("switch the element because of the nums[" + i + "] is " +  nums[i] + " the nums[" + nums[i] + "] is " + nums[nums[i]]);
+                // 这里有一个坑，我们需要记录下nums[i]中的index，
+                // 因为这个nums[i]在交换的时候会被修改，所以会导致我们交换出错。
+                // 下面被注释掉的是一段会出错的代码可以仔细思考一下为什么
+                // int temp = nums[i];
+                // nums[i] = nums[nums[i]];
+                // nums[nums[i]] = temp;
                 int index = nums[i];
                 int temp = nums[i];
                 nums[i] = nums[index];
---
+++ src/pers/ma/jianzhioffer/SearchElementInOrdered2DArray.java
@@ -0,0 +1,65 @@
+package pers.ma.jianzhioffer;
+
+/**
+ * 面试题4
+ *
+ * 问题：在二维有序数组中查找指定的元素，该数组从左到右升序，从上到下升序
+ *
+ * 例子：在下面的数组中查找元素5是否存在
+ *  1 2 3 4
+ *  5 6 7 8
+ *  9 10 11 12
+ *
+ *  解决方案：
+ *  首先这个问题的解法有很多，这里讲一个比较简单的，我们从第一行最后一列开始往前追溯我们要查找的数字
+ * （想想为什么这里要从这个位置开始而不是1行1列，见tips），首先判断当前数组位置中的内容是否跟我们的要寻找的数字相同，
+ *  如果当前的数字比目标数字大的话我们就向左走一步，否则的话就向下走一步。
+ *  tips：这里不用1行1列作为起始点的话是因为其右边和下边都是比1大的我们无法确定单一的方向
+ *
+ *
+ *  时间复杂度：
+ *  O(n) 空间复杂度：O(1)
+ *
+ * @author mabc
+ * @date 2020/4/20
+ */
+public class SearchElementInOrdered2DArray {
+    public static boolean findElement(int[][] orderedArray, int target){
+        //保存行列数
+        int row = orderedArray.length;
+        int col = orderedArray[0].length;
+        //存放最终结果，用一个result来保存结果主要是为了保持函数单一出口
+        boolean result = false;
+
+        //i,j分别是数组的当前的行和列
+        int i,j;
+
+        //开始查找
+        for (i = 0, j = col - 1; i < row  && j >= 0; ){
+
+            if (orderedArray[i][j] == target) {
+                //case1: 假设当前位置中的数据就是我们的目标值直接返回
+                System.out.println("找到目标值,目标值在[" + i + "," + j + "]");
+                result = true;
+                break;
+            }else if (orderedArray[i][j] < target) {
+                //case2: 假设当前位置中的数据比我们的目标数值要小的话，我们向下走一步
+                System.out.println("当前的位置是：[" + i + "," + j + "]，即将向下走一步！");
+                i++;
+            }else {
+                //case3：假设当前位置中的数据比我们的目标数值要大的话，我们向左走一步
+                System.out.println("当前的位置是：[" + i + "," + j + "]，即将向左走一步！");
+                j--;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args)
+    {
+        int[][] orderedArray = { {1,2,3,4},
+                                 {5,6,7,8},
+                                 {9,10,11,12} };
+
+        System.out.println(findElement(orderedArray,5));
+    }
+}
